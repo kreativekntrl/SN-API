43c0b015de1f498ee569979e3dda43d8c15ac4ea
Ava <avamsteimle@gmail.com>
2022-02-25 14:26:00 -0600

    User model done

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const {
+    Schema,
+    model
+} = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+    },
+    thoughts: [thoughSchema],
+    friends: [userSchema],
+}, {
+    toJSON: {
+        getters: true,
+    },
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
